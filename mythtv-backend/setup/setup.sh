c612345e33b03249e56301a57cb92e99bfcb4656
John Goerzen <jgoerzen@complete.org>
2017-06-12 15:06:06 -0500

    add jwplayer

--- mythtv-backend/setup/setup.sh
+++ mythtv-backend/setup/setup.sh
@@ -5,9 +5,9 @@ set -x
 
 cd /tmp/setup
 wget http://www.deb-multimedia.org/pool/main/d/deb-multimedia-keyring/deb-multimedia-keyring_2016.8.1_all.deb
+wget ftp://ftp.osuosl.org/pub/mythtv/3rdParty/jwplayer.zip
 sha256sum -c < sums
 dpkg -i deb-multimedia-keyring_2016.8.1_all.deb
 echo 'deb http://www.deb-multimedia.org stretch main non-free' >> /etc/apt/sources.list
-
-exec rm -rf /tmp/setup
+rm deb-multimedia-keyring_2016.8.1_all.deb
 
